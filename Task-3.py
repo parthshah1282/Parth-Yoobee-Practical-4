7a0efa90807355a6f2a51a38cad68c9a6e274e78
parthshah1282 <parthshah1282@gmail.com>
2025-05-01 21:39:57 +1200

    Create Task-3.py

---
+++ Task-3.py
@@ -0,0 +1,20 @@
+class Factorial:
+    def factorial(num):
+        result = 1
+        for i in range(1, num + 1):
+            result *= i
+        return result
+    def check_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+    def display(num):
+        print("Factorial of", num, "is", Factorial.factorial(num))
+        if Factorial.check_prime(num):
+            print(f"{num} is a prime number.")
+        else:
+            print(f"{num} is not a prime number.")
+Factorial.display(10)
